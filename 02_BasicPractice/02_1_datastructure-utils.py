0d83537e6d5ebe7f6d6e8f759a5fccf613e9b693
yoonkt200 <yoonkt200@gmail.com>
2018-06-16 01:31:37 +0900

    9th upload

--- 02_BasicPractice/02_1_datastructure-utils.py
+++ 02_BasicPractice/02_1_datastructure-utils.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 10 00:31:49 2018
 
-@author: yoon
-"""
+
+'''
+
+list, map, lambda 등 유용한 자료구조, 객체구조 활용법
+
+'''
+
 
 # 리스트 요소들에 2 곱하기
 def two_times(numberList):
@@ -16,9 +19,13 @@ def two_times(numberList):
 result = two_times([1, 2, 3, 4])
 print(result)
 
+
 # map 함수 이용
-def two_times(x): return x*2
+def two_times(x): 
+    return x*2
+
 list(map(two_times, [1, 2, 3, 4]))
+
 
 # map + lambda 함수 이용
 list(map(lambda a: a*2, [1, 2, 3, 4]))
@@ -34,18 +41,17 @@ def positive(l):
 
 print(positive([1,-3,2,0,-5,6]))
 
-# filter 함수 이용
-def positive(x):
-    return x > 0
 
+# filter 함수 이용
+def positive(x): return x > 0
 print(list(filter(positive, [1, -3, 2, 0, -5, 6])))
+
 
 # filter + lambda 함수 이용
 print(list(filter(lambda x: x > 0, [1, -3, 2, 0, -5, 6])))
 
 
-
-# 예제
+# 연습!
 x = [2, 3, 4, 5, 6]
 y = map(lambda v : v * 5, filter(lambda u : u % 2, x))
 
@@ -54,3 +60,10 @@ for i in y:
     
 print(list(filter(lambda u : u % 2, x)))
 print(list(filter(lambda u : u < 4, x)))
+
+
+# dictionary 자료구조
+my_dict = {'first': 1, 'second': '2'}
+for key, val in my_dict.items():
+    print(key)
+    print(val)
--- 02_BasicPractice/02_2_string-generator-utils.py
+++ 02_BasicPractice/02_2_string-generator-utils.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Dec  8 23:42:36 2017
 
-@author: yoon
-"""
 
-import timeit
+'''
+
+string을 concat하는 여러가지 방법
+
+'''
+
 
 # only python2.x
 def method1(str_arr):
@@ -15,12 +16,14 @@ def method1(str_arr):
         out_str += num
     return out_str
 
+
 # python3.x
 def method1(str_arr):
     out_str = ''
     for num in range(len(str_arr)):
         out_str += str_arr[num]
     return out_str
+
 
 # only python2.x
 def method2(str_arr):
@@ -30,15 +33,19 @@ def method2(str_arr):
         char_array.fromstring(str_arr[num])
     return char_array.tostring()
 
-def method4(str_arr):
+
+def method2(str_arr):
     from io import StringIO
     file_str = StringIO()
     for num in range(len(str_arr)):
         file_str.write(str_arr[num])
     return file_str.getvalue()
 
+
 hello = "hello"
 world = "world"
+
+method2(hello)
 
 print (hello + " " + world)
 print (hello, world)
@@ -46,10 +53,15 @@ print ("%s %s" % (hello, world))
 print ("{} {}".format(hello, world))
 print (' '.join([hello, world]))
 
-start = timeit.default_timer()
 method2(["awdaw", "awwss", "dddd"])
-stop = timeit.default_timer()
-print(stop - start)
+
+
+
+'''
+
+generator에 대한 설명 및 예제
+
+'''
 
 def generator(n):
     i = 0
@@ -61,8 +73,36 @@ for x in generator(5):
     print (x)
 
 
+'''
+
+generator가 머신 러닝에 적용되는 예제
+
+'''
+
+### 문서를 스트리밍 형식으로 읽도록 하는 함수
+def stream_docs(dataset):
+    for row in dataset:
+        text, label = row[0], row[1]
+        yield text, label
 
 
+### 미니배치를 얻어오는 함수
+def get_minibatch(doc_stream, size):
+    docs, y = [], []
+    try:
+        for _ in range(size):
+            text, label = next(doc_stream)
+            docs.append(text)
+            y.append(label)
+    except StopIteration:
+        return None, None
+    return docs, y
 
 
+### N x 2 의 형태로 되어있는 데이터셋
+doc_stream = stream_docs({your data})
 
+
+### Some codes/logics here
+for _ in range(10):
+    X_train, y_train = get_minibatch(doc_stream, size=500)
