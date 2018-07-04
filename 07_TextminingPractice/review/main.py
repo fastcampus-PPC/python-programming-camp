c00594e73bf0e5ba5f11e1e457bd7c9bd58c56af
yoonkt200 <yoonkt200@gmail.com>
2018-07-04 23:56:05 +0900

    upload

--- 07_TextminingPractice/review/main.py
+++
@@ -1,8 +0,0 @@
-from pretrained.loader.pkl_loader import Vectorizer, Classifier
-
-if __name__ == "__main__":
-    review = input("Please enter your review: ")
-    input_features = Vectorizer.instance().get_vector(review)
-    result = Classifier.instance().get_prediction(input_features)
-
-    print("당신은 " + str(result))
